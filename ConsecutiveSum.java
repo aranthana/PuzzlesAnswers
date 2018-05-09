b624a1a045518034a83ecddc818732a720055061
aranya_thanabalasingam <aru2191@hmail.com>
2018-05-09 13:13:51 +0530

    commit

--- ConsecutiveSum.java
+++ ConsecutiveSum.java
@@ -4,9 +4,16 @@ public class ConsecutiveSum {
 	static int consecutiveCount = 0;
 	
 	static int consecutive(long num) {
-		int triangleNum=0;
+		long triangleNum=0;
+		long size=0;
+		if(num % 2 != 0){								// if num is a odd number
+	        size = num / 2 + 1;
+	    }
+	    else{											// if num is a even number
+	        size = num / 2;
+	    }
 		
-		for(int i=1; i<num/2;i++){
+		for(int i=1; i<size;i++){
 			triangleNum=i*(i+1)/2;								// find i th triangle number
 			if(triangleNum<num){
 				if((num-triangleNum)%(i+1)==0){					
