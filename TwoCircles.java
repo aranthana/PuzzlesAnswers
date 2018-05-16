01dd0a8e2e6ffec32e4329f7ee0863346a8e2d2a
aranthana <aru2191@gmail.com>
2018-05-16 14:19:18 +0530

    Update TwoCircles.java

--- TwoCircles.java
+++ TwoCircles.java
@@ -2,110 +2,59 @@ import java.util.Scanner;
 
 public class TwoCircles {
 	
-	public static String[] circles(String[] info) {
-		String [] results=new String [info.length];				 	// info array
-		for(int i=0;i<info.length;i++){	
-			//System.out.println(info[1]+info.length);
-			 String[] infoStr = info[i].split(" ");
-	            int[] infoInt = new int[infoStr.length];
-	           // System.out.println(infoStr[5]);
-	            for (int j=0;j<infoStr.length;j++) infoInt[j]=Integer.valueOf(infoStr[j]);
-			
-	            if(infoInt[0]==0 && infoInt[3]==0){			// circles with x=0
-				if(infoInt[1]==infoInt[4]){				// both center at a point
-					results[i]="Concentric";
-				}
-				else{
-				
-					int circleRad_1=infoInt[2];
-					int circleRad_2=infoInt[5];
-					
-					if(infoInt[1]>infoInt[4]){				// if center of circle_one  above the center of circle_two
-						//if(circleBottomPt_1==circleTopPt_2 || circleBottomPt_1==circleBottomPt_2 || circleTopPt_1==circleTopPt_2){
-						if((circleRad_1+circleRad_2==infoInt[1]-infoInt[4]) ||(Math.abs(circleRad_2-circleRad_1)== infoInt[1]-infoInt[4])){	
-						results[i]="Touching";
-						}
-						
-						else if(circleRad_1+circleRad_2<infoInt[1]-infoInt[4]){
-							results[i]="Disjoint‐Outside";
-						}
-						else{
-							if(Math.abs(circleRad_1-circleRad_2)>infoInt[1]-infoInt[4] ){
-								results[i]="Disjoint‐Inside";
-								
-							}else{
-								results[i]="Intersecting";
-							}
-							
-						}
-					}else{								// if center of circle_two  above the center of circle_one
-						if((circleRad_1+circleRad_2==infoInt[4]-infoInt[1])||(Math.abs(circleRad_1-circleRad_2)== infoInt[4]-infoInt[1])){
-							results[i]="Touching";
-						}
-						else if(circleRad_1+circleRad_2<infoInt[4]-infoInt[1]){
-							results[i]="Disjoint‐Outside";
-						}
-						else{
-							if(Math.abs(circleRad_1-circleRad_2)>infoInt[4]-infoInt[1] ){
-								results[i]="Disjoint‐Inside";
-								
-							}else{
-								results[i]="Intersecting";
-							}
-							
-						}
-					}
-				}
-			}
-			if(infoInt[1]==0 && infoInt[4]==0){				// circles with y=0
-				if(infoInt[0]==infoInt[3]){					// centers of circles at a point
-					results[i]="Concentric";
-				}else{
-					
-					int circleRad_1=infoInt[2];
-					int circleRad_2=infoInt[5];
-					
-							
-					if(infoInt[0]>infoInt[3]){			// if center of circle_one  right to the center of circle_two
-						if((circleRad_1+circleRad_2==infoInt[0]-infoInt[3])|| (Math.abs(circleRad_2-circleRad_1)== infoInt[0]-infoInt[3])){
-							results[i]="Touching";
-						}
-						else if(circleRad_1+circleRad_2<infoInt[0]-infoInt[3]){
-							results[i]="Disjoint‐Outside";
-						}
-						else{
-							if(Math.abs(circleRad_1-circleRad_2)>infoInt[0]-infoInt[3] ){
-								results[i]="Disjoint‐Inside";
-								
-							}else{
-								results[i]="Intersecting";
-							}
-							
-						}
-					}else{								// if center of circle_two  right to the center of circle_one
-						if((circleRad_1+circleRad_2==infoInt[3]-infoInt[0])|| (Math.abs(circleRad_1-circleRad_2)== infoInt[3]-infoInt[0])){
-							results[i]="Touching";
-						}
-						else if(circleRad_1+circleRad_2<infoInt[3]-infoInt[0]){
-							results[i]="Disjoint‐Outside";
-						}
-						else{
-							if(Math.abs(circleRad_1-circleRad_2)>infoInt[3]-infoInt[0] ){
-								results[i]="Disjoint‐Inside";
-								
-							}else{
-								results[i]="Intersecting";
-							}
-							
-						}
-					}
-				}		
-			}
-		}
-		
-		return results;
-    }
+	 private static final String touching = "Touching";
+   
+ private static final String concentric = "Concentric";
+    
+private static final String intersecting = "Intersecting";
+  
+  private static final String dis_out = "Disjoint‐Outside";
+   
+ private static final String dis_in = "Disjoint‐Inside";
 
+   
+
+ static String[] circles(String[] info) {
+
+    
+    /*
+        System.out.println("circle information:");
+        for (String s:info) {
+            System.out.println(s);
+        }
+        System.out.println("-------------------");
+        */
+    
+
+    int l = info.length;
+
+       
+ String[] relations = new String[l];
+   
+     for (int n=0;n<l;n++) {
+         
+   String[] infoStr = info[n].split(" ");
+   
+         int[] infoInt = new int[infoStr.length];
+    
+        for (int i=0;i<infoStr.length;i++) infoInt[i]=Integer.valueOf(infoStr[i]);
+
+   
+         if (infoInt[0]==infoInt[3]&&infoInt[1]==infoInt[4])
+   
+             relations[n]=concentric;
+            else {
+                int distance = Math.abs(infoInt[0]-infoInt[3]+infoInt[1]-infoInt[4]);
+                int radiusSum = infoInt[2]+infoInt[5];
+                int radiusDiff = Math.abs(infoInt[2]-infoInt[5]);
+                if (radiusDiff==distance||radiusSum==distance) relations[n]=touching;
+                else if (radiusDiff<distance&&radiusSum>distance) relations[n]=intersecting;
+                else if (radiusDiff>distance) relations[n]=dis_in;
+                else relations[n]=dis_out;
+            }
+        }
+        return relations;
+ 
 
 		
 }
